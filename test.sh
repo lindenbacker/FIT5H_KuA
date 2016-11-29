6fca426d11cfdf0b9b8ef9b79333fae980111c7b
lindenbacker <hendrik.schulte@svsurwold.de>
2016-11-29 14:50:22 +0100

    weil ich es kann

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+trinken='whisky und zigarre'
+echo $trinken
+
+echo $0
+echo $2
+
+echo 'date'
+echo $(date)
+
+remove='rm test.txt'
+$remove
+
+string_1='Heia und'
+string_2='popei'a
+sting="${string_1} ${sting_2} was raschelt im Stroh?"
+echo "$string_1 $string_2 was raschelt im Stroh?"
+echo $string
